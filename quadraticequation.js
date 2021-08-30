3fcacef507e4efe1c944045abf598591c56ca071
RoyRiemer <89573231+RoyRiemer@users.noreply.github.com>
2021-08-30 22:13:24 +0300

    quadratic equation solution(2)
    solving a quadratic equation

---
+++ quadraticequation.js
@@ -0,0 +1,8 @@
+const a = 1; b = -6; c = 9;  // stands for x**2 - 6*x + 9 = 0
+let solutionA = solutionB = 0
+// your code goes here
+
+d = (a**2) - (4*b*c);
+solutionA = (-b-Math.sqrt(d))/(2*a); 
+solutionB = (-b+Math.sqrt(d))/(2*a);
+console.log(solutionA, solutionB);

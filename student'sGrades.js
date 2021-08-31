8bf6acff6df5c3610677b892f199a5f398f884e1
RoyRiemer <89573231+RoyRiemer@users.noreply.github.com>
2021-08-31 06:47:35 +0300

    second half range solution

--- student'sGrades.js
+++ student'sGrades.js
@@ -4,5 +4,10 @@ let lowest=Math.min.apply(null,grades);
 let highest=Math.max.apply(null,grades);
 let range=highest-lowest;
 console.log(range);
+let gradesSlice=grades.slice(5)
 let gradesNew=grades.sort(function(a,b){return a-b})
 console.log(gradesNew[4]);
+let lowestSlice=Math.min.apply(null,gradesSlice);
+let highestSlice=Math.max.apply(null,gradesSlice);
+let rangeSlice=highestSlice-lowestSlice;
+console.log(rangeSlice);

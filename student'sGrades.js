3c69b8cb49815d2984ada6b3960a1cf3b34b8a5e
RoyRiemer <89573231+RoyRiemer@users.noreply.github.com>
2021-08-31 06:00:58 +0300

    range slution
    finding the range of the grades

---
+++ student'sGrades.js
@@ -0,0 +1,6 @@
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+// your code goes here
+let lowest=Math.min.apply(null,grades);
+let highest=Math.max.apply(null,grades);
+let range=highest-lowest;
+console.log(range);

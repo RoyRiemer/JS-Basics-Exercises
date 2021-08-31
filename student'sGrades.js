603a29f9f6312aa6a078e9d7e453708456eafef3
RoyRiemer <89573231+RoyRiemer@users.noreply.github.com>
2021-08-31 06:54:09 +0300

    fixing the format

--- student'sGrades.js
+++ student'sGrades.js
@@ -1,13 +1,14 @@
 const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
 // your code goes here
+console.log("stats:");
 let lowest=Math.min.apply(null,grades);
 let highest=Math.max.apply(null,grades);
 let range=highest-lowest;
-console.log(range);
+console.log("Range:",range);
 let gradesSlice=grades.slice(5)
 let gradesNew=grades.sort(function(a,b){return a-b})
-console.log(gradesNew[4]);
+console.log("Median:",gradesNew[4]);
 let lowestSlice=Math.min.apply(null,gradesSlice);
 let highestSlice=Math.max.apply(null,gradesSlice);
 let rangeSlice=highestSlice-lowestSlice;
-console.log(rangeSlice);
+console.log("Half Range:",rangeSlice);
